e269e95936b3e5ad31fd06642d4498616d10d9fd
Johel Hidalgo <johelhu@gmail.com>
2023-09-14 11:44:34 -0600

    Pruebas en el entorno kivy, ademá de agregar main.kv

--- interfaz_prueba.py
+++ interfaz_prueba.py
@@ -1,10 +1,24 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+import kivy #Libreria de kive
+from kivy.app import App #Lugar para vaciar los widgets
+from kivy.uix.boxlayout import BoxLayout # Forma en la que se va a ver
+from kivy.config import Config #Poder hacer configuraciones
+#Config.set('graphics', 'width', 400 #configurar el alto de la ventana
+#Config.set('graphics', 'height', 400 #configurar altura de la ventana
 
-class FirstApp(App):
-    def build(self):
-        return Label(text = "prueba using name/main")
+class Box01(BoxLayout): #Declaración de nuestro Widget
+    None #Accion a realizar en este caso ninguna
+    
+class MainApp(App): #Llamar a nuestro archivo .kv (main.kv) para darle las apariencias a los widgets
+    title = "Hola Mundo" #Nombre de la aplicacion
+    def build(self): #Lo primero que busca kiby para saber que hacer
+        return Box01() #Le decimos que nos regrese (Contenedor_01)
+        
+if __name__== '__main__': #Condición para ejecutar
+    MainApp().run() #Correr la aplicación
 
-if __name__== "__main__":
-    FirstApp().run()
+#class FirstApp(App):
+#    def build(self):
+#        return Label(text = "prueba using name/main")
+#
+#if __name__== "__main__":
+#    FirstApp().run()
